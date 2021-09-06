24eaa480537d5b5360e78a921790fa1269c61439
felipecabesssa <felipesoares100@hotmail.com>
2021-09-06 19:30:56 -0300

    Rodapé

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-primary">
-        Olá mundo!
-      </h1>
-    </div>
+  return (    
+      <>
+        <NavBar />
+        <div className="container">
+        <h1 className="text-primary">
+          Olá mundo!
+        </h1>
+        </div>
+        <Footer></Footer>
+      </>
+    
+
+    
   );
 }
 
