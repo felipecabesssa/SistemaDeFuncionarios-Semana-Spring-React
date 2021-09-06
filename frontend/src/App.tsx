89d4ad9f1e6b3a6275978c39947733718daba852
felipecabesssa <felipesoares100@hotmail.com>
2021-09-06 19:38:27 -0300

    Tabela de dados - Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -6,9 +7,10 @@ function App() {
       <>
         <NavBar />
         <div className="container">
-        <h1 className="text-primary">
-          Olá mundo!
-        </h1>
+        <h1 className="text-primary">Olá mundo!</h1>
+
+        <DataTable />
+
         </div>
         <Footer></Footer>
       </>
