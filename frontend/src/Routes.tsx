997c95406c7acf94d205d5bd3cce4c5a856d2671
felipecabesssa <felipesoares100@hotmail.com>
2021-09-13 23:31:19 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,24 @@
+import Home from 'pages/Home';
+import Dashboard from 'pages/Home/Dashboard';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+        
+            <Switch>
+
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard" exact>
+                    <Dashboard />
+                </Route>
+
+            </Switch>
+        
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
